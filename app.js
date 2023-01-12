5db27a10f27a538e42bb00e7eec86660341b46dd
Hsin-tingCHI <chihsinting@gmail.com>
2023-01-12 14:29:44 +0100

    feat: add delete restaurant function

--- app.js
+++ app.js
@@ -85,6 +85,15 @@ app.post('/restaurants/:restaurant_id/edit', (req, res) => {
         .catch(error => console.log(error));
 });
 
+// delete restaurant
+app.post('/restaurants/:restaurant_id/delete', (req, res) => {
+    const id = req.params.restaurant_id;
+    return Restaurant.findById(id)
+        .then(restaurant => restaurant.remove())
+        .then(() => res.redirect('/'))
+        .catch(error => console.log(error));
+});
+
 // * not refactored yet
 // show search results 
 app.get('/search', (req, res) => {
