762e25f4df6a9306689578cf227eff3bde1e1586
Hsin-tingCHI <chihsinting@gmail.com>
2023-01-13 11:00:25 +0100

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -7,22 +7,23 @@ const methodOverride = require('method-override');
 // require restaurant model
 const Restaurant = require('./models/restaurant');
 const routes = require('./routes');
+require('./config/mongoose');
 
 // mongoose stuff
-const mongoose = require('mongoose');
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config();
-}
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-mongoose.set('strictQuery', true);
+// const mongoose = require('mongoose');
+// if (process.env.NODE_ENV !== 'production') {
+//     require('dotenv').config();
+// }
+// mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+// mongoose.set('strictQuery', true);
 
-const db = mongoose.connection;
-db.on('error', () => {
-    console.log('mongodb error!');
-});
-db.once('open', () => {
-    console.log('mongodb connected!');
-});
+// const db = mongoose.connection;
+// db.on('error', () => {
+//     console.log('mongodb error!');
+// });
+// db.once('open', () => {
+//     console.log('mongodb connected!');
+// });
 
 // app related stuff
 const app = express();
