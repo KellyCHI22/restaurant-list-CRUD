2a12e5cd8022440fddd4fb0aa18a7466ff67ad2a
Hsin-tingCHI <chihsinting@gmail.com>
2023-07-06 15:22:46 +0200

    feat: users can only see their own restaurant list

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -4,66 +4,74 @@ const Restaurant = require('../../models/restaurant');
 
 // default sorting: id
 router.get('/', (req, res) => {
-    Restaurant.find()
-        .lean()
-        .sort({ _id: 'asc' })
-        .then(restaurants => {
-            const message = `您的清單中有${restaurants.length}間餐廳`;
-            res.render('index', { restaurants, message });
-        })
-        .catch(error => console.error(error));
+  const userId = req.user._id;
+  Restaurant.find({ userId })
+    .lean()
+    .sort({ _id: 'asc' })
+    .then((restaurants) => {
+      const message = `您的清單中有${restaurants.length}間餐廳`;
+      res.render('index', { restaurants, message });
+    })
+    .catch((error) => console.error(error));
 });
 
 // sorting function
 router.get('/name', (req, res) => {
-    Restaurant.find()
-        .lean()
-        .sort({ name: 'asc' })
-        .then(restaurants => res.render('index', { restaurants }))
-        .catch(error => console.error(error));
+  const userId = req.user._id;
+  Restaurant.find({ userId })
+    .lean()
+    .sort({ name: 'asc' })
+    .then((restaurants) => res.render('index', { restaurants }))
+    .catch((error) => console.error(error));
 });
 
 router.get('/-name', (req, res) => {
-    Restaurant.find()
-        .lean()
-        .sort({ name: 'desc' })
-        .then(restaurants => res.render('index', { restaurants }))
-        .catch(error => console.error(error));
+  const userId = req.user._id;
+  Restaurant.find({ userId })
+    .lean()
+    .sort({ name: 'desc' })
+    .then((restaurants) => res.render('index', { restaurants }))
+    .catch((error) => console.error(error));
 });
 
 router.get('/category', (req, res) => {
-    Restaurant.find()
-        .lean()
-        .sort({ category: 'asc' })
-        .then(restaurants => res.render('index', { restaurants }))
-        .catch(error => console.error(error));
+  const userId = req.user._id;
+  Restaurant.find({ userId })
+    .lean()
+    .sort({ category: 'asc' })
+    .then((restaurants) => res.render('index', { restaurants }))
+    .catch((error) => console.error(error));
 });
 
 router.get('/location', (req, res) => {
-    Restaurant.find()
-        .lean()
-        .sort({ location: 'asc' })
-        .then(restaurants => res.render('index', { restaurants }))
-        .catch(error => console.error(error));
+  const userId = req.user._id;
+  Restaurant.find({ userId })
+    .lean()
+    .sort({ location: 'asc' })
+    .then((restaurants) => res.render('index', { restaurants }))
+    .catch((error) => console.error(error));
 });
 
 router.get('/rating', (req, res) => {
-    Restaurant.find()
-        .lean()
-        .sort({ rating: 'desc' })
-        .then(restaurants => res.render('index', { restaurants }))
-        .catch(error => console.error(error));
+  const userId = req.user._id;
+  Restaurant.find({ userId })
+    .lean()
+    .sort({ rating: 'desc' })
+    .then((restaurants) => res.render('index', { restaurants }))
+    .catch((error) => console.error(error));
 });
 
 router.get('/favorite', (req, res) => {
-    Restaurant.find()
-        .lean()
-        .then(restaurants => {
-            const filteredRestaurants = restaurants.filter(restaurant => restaurant.isFavorite === true);
-            return res.render('index', { restaurants: filteredRestaurants });
-        })
-        .catch(error => console.error(error));
+  const userId = req.user._id;
+  Restaurant.find({ userId })
+    .lean()
+    .then((restaurants) => {
+      const filteredRestaurants = restaurants.filter(
+        (restaurant) => restaurant.isFavorite === true
+      );
+      return res.render('index', { restaurants: filteredRestaurants });
+    })
+    .catch((error) => console.error(error));
 });
-
 
 module.exports = router;
--- routes/modules/restaurants.js
+++ routes/modules/restaurants.js
@@ -4,54 +4,62 @@ const Restaurant = require('../../models/restaurant');
 
 // show new page
 router.get('/new', (req, res) => {
-    return res.render('new');
+  return res.render('new');
 });
 
 // create new restaurant
 router.post('/', (req, res) => {
-    return Restaurant.create(req.body)
-        .then(() => res.redirect('/'))
-        .catch(err => console.log(err));
+  const userId = req.user._id;
+  return Restaurant.create({ ...req.body, userId })
+    .then(() => res.redirect('/'))
+    .catch((err) => console.log(err));
 });
 
 // show restaurant by id
 router.get('/:restaurant_id', (req, res) => {
-    const id = req.params.restaurant_id;
-    return Restaurant.findById(id)
-        .lean()
-        .then((restaurant) => res.render('show', { restaurant }))
-        .catch(error => console.log(error));
+  const userId = req.user._id;
+  const _id = req.params.restaurant_id;
+  return Restaurant.findOne({ _id, userId })
+    .lean()
+    .then((restaurant) => res.render('show', { restaurant }))
+    .catch((error) => console.log(error));
 });
 
 // show edit restaurant page
-router.get("/:restaurant_id/edit", (req, res) => {
-    const id = req.params.restaurant_id;
-    return Restaurant.findById(id)
-        .lean()
-        .then(restaurant => res.render("edit", { restaurant }))
-        .catch(err => console.log(err));
+router.get('/:restaurant_id/edit', (req, res) => {
+  const userId = req.user._id;
+  const _id = req.params.restaurant_id;
+  return Restaurant.findOne({ _id, userId })
+    .lean()
+    .then((restaurant) => res.render('edit', { restaurant }))
+    .catch((err) => console.log(err));
 });
 
 // edit restaurant
 router.put('/:restaurant_id', (req, res) => {
-    const id = req.params.restaurant_id;
-    const editedData = {
-        ...req.body,
-        isFavorite: req.body.isFavorite === 'on' ? true : false
-    };
-    return Restaurant.findByIdAndUpdate(id, editedData)
-        .lean()
-        .then(() => res.redirect(`/restaurants/${id}`))
-        .catch(error => console.log(error));
+  const userId = req.user._id;
+  const _id = req.params.restaurant_id;
+  const editedData = {
+    ...req.body,
+    isFavorite: req.body.isFavorite === 'on' ? true : false,
+  };
+  console.log(editedData);
+  return Restaurant.findOneAndUpdate({ _id, userId }, editedData, {
+    new: true,
+  })
+    .lean()
+    .then(() => res.redirect(`/restaurants/${_id}`))
+    .catch((error) => console.log(error));
 });
 
 // delete restaurant
 router.delete('/:restaurant_id', (req, res) => {
-    const id = req.params.restaurant_id;
-    return Restaurant.findById(id)
-        .then(restaurant => restaurant.remove())
-        .then(() => res.redirect('/'))
-        .catch(error => console.log(error));
+  const userId = req.user._id;
+  const _id = req.params.restaurant_id;
+  return Restaurant.findOne({ _id, userId })
+    .then((restaurant) => restaurant.remove())
+    .then(() => res.redirect('/'))
+    .catch((error) => console.log(error));
 });
 
 module.exports = router;
