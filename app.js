42df87e812d102dab9c6dc63eae116a290a70ded
Hsin-tingCHI <chihsinting@gmail.com>
2023-07-06 15:22:26 +0200

    feat: add message partial

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
+const flash = require('connect-flash');
 
 // require restaurant model
 const Restaurant = require('./models/restaurant');
@@ -34,10 +35,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
 usePassport(app);
+app.use(flash());
 
 app.use((req, res, next) => {
   res.locals.isAuthenticated = req.isAuthenticated();
   res.locals.user = req.user;
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.warning_msg = req.flash('warning_msg');
   next();
 });
 
