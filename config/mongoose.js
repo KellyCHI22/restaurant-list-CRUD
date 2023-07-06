3421d4cc128ba2d3cc4ddaeb801c455ad50cf93f
Hsin-tingCHI <chihsinting@gmail.com>
2023-07-06 14:14:03 +0200

    feat: add user model and modify restaurant model

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,18 +1,21 @@
 const mongoose = require('mongoose');
 if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config();
+  require('dotenv').config();
 }
 
 mongoose.set('strictQuery', false);
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-
+console.log(process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const db = mongoose.connection;
 db.on('error', () => {
-    console.log('mongodb error!');
+  console.log('mongodb error!');
 });
 db.once('open', () => {
-    console.log('mongodb connected!');
+  console.log('mongodb connected!');
 });
 
 module.exports = db;
